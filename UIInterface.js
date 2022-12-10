a8ab14289245f75fd8a6eb1995f2737014880fcf
Piyush Terkar <terkar.piyush3@gmail.com>
2022-12-10 12:50:10 +0530

    UI interface

---
+++ UIInterface.js
@@ -0,0 +1,47 @@
+const select = document.querySelector("select");
+
+const desktop = document.querySelector("#desktop");
+const webApp = document.querySelector("#webapp");
+const mobile = document.querySelector("#mobile");
+const ecosystem = document.querySelector("#ecosystem");
+
+desktop.addEventListener("click", () => {
+  select.value = "Desktop App";
+});
+
+webApp.addEventListener("click", () => {
+  select.value = "Web App";
+});
+
+mobile.addEventListener("click", () => {
+  select.value = "Mobile App";
+});
+
+ecosystem.addEventListener("click", () => {
+  select.value = "Software Ecosystem";
+});
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  "use strict";
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  const forms = document.querySelectorAll(".validated-form");
+
+  // Loop over them and prevent submission
+  Array.from(forms).forEach(function (form) {
+    form.addEventListener(
+      "submit",
+      function (event) {
+        if (!form.checkValidity()) {
+          console.log("HITTT");
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add("was-validated");
+      },
+      false
+    );
+  });
+})();
